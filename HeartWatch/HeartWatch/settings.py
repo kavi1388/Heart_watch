a3da362b72e4fa8f108124d8a4b2cc66e33fc684
rajnish-1292 <rajnish@bnsanalytics.com>
2021-10-21 19:19:59 +0530

    allow host all

--- HeartWatch/HeartWatch/settings.py
+++ HeartWatch/HeartWatch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-td^!_o3o3a9v@c5rht4nm$qe=s_iax4&tyx^@8@ejxw(rxe8ur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
