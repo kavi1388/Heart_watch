7ac8a154e1518b16911a811fac4378719afe77a2
rajnish-1292 <rajnish@bnsanalytics.com>
2021-10-23 22:16:47 +0530

    some changes

--- HeartWatch/HeartWatch/settings.py
+++ HeartWatch/HeartWatch/settings.py
@@ -167,8 +167,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/home/heartwatch/rajnishh/Heart_watch/HeartWatch/static"
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
