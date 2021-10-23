d2fe540978dc429b507e4e64d2835a23253e98fb
rajnish-1292 <rajnish@bnsanalytics.com>
2021-10-23 22:32:43 +0530

    some changes

--- HeartWatch/HeartWatch/settings.py
+++ HeartWatch/HeartWatch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-td^!_o3o3a9v@c5rht4nm$qe=s_iax4&tyx^@8@ejxw(rxe8ur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['164.52.214.242']
 
