af2d8f1005999437aad7a6d0fd40d37ef9fe2092
Kavitha V <kavitha.viswanathan@heartwatch.in>
2021-10-30 18:40:48 +0530

    new version (ppg_sig, time_val, fl=0.4, fh=3.5, o=4, n=10, diff_max=4, r=1)

--- HeartWatch/heart_rate/views.py
+++ HeartWatch/heart_rate/views.py
@@ -351,7 +351,7 @@ class HeartRateDetail(APIView):
                     data_valid = False
         if data_valid:
             final_pr, ppg_21, ppg_sig, ppg_bpf, t_diff_afib, hr_extracted, peaks_all2, non_uniform = ppg_plot_hr(
-                ppg_sig, time_val, fl=0.4, fh=3.5, o=4, n=10, diff_max=4, r=1))
+                ppg_sig, time_val, fl=0.4, fh=3.5, o=4, n=10, diff_max=4, r=1)
 
             for i in range(len(hr_extracted)):
                 if 60 > hr_extracted[i] >= 40:
