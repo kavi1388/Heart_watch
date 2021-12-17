9c4484b24a59651fd5fb305a617fc99a8e5ce921
Kavitha V <kavitha.viswanathan@heartwatch.in>
2021-12-17 18:23:35 +0530

    New APIs for android debugging

--- HeartWatch/heart_rate/ppg_ailments.py
+++ HeartWatch/heart_rate/ppg_ailments.py
@@ -91,6 +91,7 @@ def ppg_plot_hr(ppg_sig, time_val, fl=1, fh=5, o=4, n=5, diff_max=4, r=1):
     t_diff_afib = []
     rr_interval = []
     spo2_pred = []
+    non_uniform = 0
     for i in range(n, len(ppg_sig) + n, jump):
 
         if i > len(ppg_sig) - 1:
@@ -111,7 +112,7 @@ def ppg_plot_hr(ppg_sig, time_val, fl=1, fh=5, o=4, n=5, diff_max=4, r=1):
                 c.append(p * 20)
         peaks, _ = find_peaks(c, height=np.var(ppg_21, ddof=1) / 3, distance=11)
         peaks_all2, _ = find_peaks(ppg_21, height=np.mean(ppg_21), distance=11)
-        non_uniform = 0
+
         for a in range(len(peaks_all2) - 1):
             if len(rr_interval) > 0 and 399 < (peaks_all2[a + 1] - peaks_all2[a]) * 1000 / fs < 1800.0:
                 rr_interval.append((peaks_all2[a + 1] - peaks_all2[a]) * 1000 / fs)
