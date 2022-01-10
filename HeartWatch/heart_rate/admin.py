a96fcbe72875dddb49bfad98124696fe21c539ef
Kavitha V <kavitha.viswanathan@heartwatch.in>
2022-01-10 14:26:51 +0530

    API admin.py modified

--- HeartWatch/heart_rate/admin.py
+++ HeartWatch/heart_rate/admin.py
@@ -1,42 +1,42 @@
 from django.contrib import admin
 from .models import *
-from import_export.admin import ExportActionMixin
+# from import_export.admin import ExportActionMixin
 
 
 # Register your models here.
 
-class PPG_data_from_AndroidAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'heart_rate_voltage')
-
-
-class Accelerometer_data_from_AndroidAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'Accelerometer')
-
-
-# Register your models here.
-class PPG_DataAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'heart_rate_voltage')
-
-
-class Accelerometer_dataAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'Accelerometer')
-
-
-class PPG_data_newAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'user_id', 'heart_rate_voltage')
-
-
-class Accelerometer_data_newAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'user_id', 'Accelerometer')
-
-
-admin.site.register(PPG_result_save)
-admin.site.register(Accelerometer_result_save)
-admin.site.register(PPG_data, PPG_DataAdmin)
-admin.site.register(PPG_data_new, PPG_data_newAdmin)
-admin.site.register(Accelerometer_data, Accelerometer_dataAdmin)
-admin.site.register(Accelerometer_data_new, Accelerometer_data_newAdmin)
-
-
-admin.site.register(PPG_data_from_Android, PPG_data_from_AndroidAdmin)
-admin.site.register(Accelerometer_data_from_Android, Accelerometer_data_from_AndroidAdmin)
+# class PPG_data_from_AndroidAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('id', 'heart_rate_voltage')
+#
+#
+# class Accelerometer_data_from_AndroidAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('id', 'Accelerometer')
+#
+#
+# # Register your models here.
+# class PPG_DataAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('id', 'heart_rate_voltage')
+#
+#
+# class Accelerometer_dataAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('id', 'Accelerometer')
+#
+#
+# class PPG_data_newAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('id', 'user_id', 'heart_rate_voltage')
+#
+#
+# class Accelerometer_data_newAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('id', 'user_id', 'Accelerometer')
+#
+#
+# admin.site.register(PPG_result_save)
+# admin.site.register(Accelerometer_result_save)
+# admin.site.register(PPG_data, PPG_DataAdmin)
+# admin.site.register(PPG_data_new, PPG_data_newAdmin)
+# admin.site.register(Accelerometer_data, Accelerometer_dataAdmin)
+# admin.site.register(Accelerometer_data_new, Accelerometer_data_newAdmin)
+#
+#
+# admin.site.register(PPG_data_from_Android, PPG_data_from_AndroidAdmin)
+# admin.site.register(Accelerometer_data_from_Android, Accelerometer_data_from_AndroidAdmin)
